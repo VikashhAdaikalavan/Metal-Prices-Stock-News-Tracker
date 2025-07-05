c1d8f582d981b3dabadcc65d6ca1497e557169f0
VikashhAdaikalavan <vikashhadaikalavan@gmail.com>
2025-07-05 11:41:54 +0530

    Added bart model for summarizing articles

---
+++ llm.py
@@ -0,0 +1,11 @@
+from transformers import BartTokenizer, BartForConditionalGeneration
+
+model_name = "sshleifer/distilbart-cnn-12-6"
+tokenizer = BartTokenizer.from_pretrained(model_name)
+model = BartForConditionalGeneration.from_pretrained(model_name)
+
+def summarize(article):
+    inputs = tokenizer([article], max_length=1024, return_tensors="pt", truncation=True)
+    summary_ids = model.generate(inputs["input_ids"],max_length=150,min_length=40,length_penalty=2.0,num_beams=4,early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
