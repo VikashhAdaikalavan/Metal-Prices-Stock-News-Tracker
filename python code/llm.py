2fe5fb612063daf9e08d9c3b43a9771f75afa69c
VikashhAdaikalavan <vikashhadaikalavan@gmail.com>
2025-07-05 17:30:50 +0530

    Added Scrapy

--- python code/llm.py
+++ python code/llm.py
@@ -9,3 +9,4 @@ def summarize(article):
     summary_ids = model.generate(inputs["input_ids"],max_length=150,min_length=40,length_penalty=2.0,num_beams=4,early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
+
