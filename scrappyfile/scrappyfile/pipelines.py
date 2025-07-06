b3feb95852e44490f793bdcf3edc9235e36ff12f
VikashhAdaikalavan <vikashhadaikalavan@gmail.com>
2025-07-06 12:19:34 +0530

    Added some logic in Databse_Pipeline

--- scrappyfile/scrappyfile/pipelines.py
+++ scrappyfile/scrappyfile/pipelines.py
@@ -7,9 +7,11 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrappyfile.llm import summarize
+import sqlite3
 
 
 class News_Summary_Pipeline:
+    #To feed the scraped data into an LLM to get the summary if the news is longer
     def process_item(self, item, spider):
         adapted_item = ItemAdapter(item)
         adapted_item["llm_summary"] = summarize(adapted_item["summary"])
@@ -17,6 +19,12 @@ class News_Summary_Pipeline:
         return item
 
 class Database_Pipeline:
+    #To store the data in a sqlite databse
     def process_item(self,item,spider):
+        connection = sqlite3.connect("news.db")
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE IF EXISTS article")
+        cursor.execute("Create new table article()")
+
 
         return item
